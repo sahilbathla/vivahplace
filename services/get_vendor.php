53897e7a4133d134cef5cd4cf4ff60c4d309ca63
Sahil Bathla <sahilbathla1@gmail.com>
2013-08-29 20:10:37 +0530

    Services added get vendor by id

--- services/get_vendor.php
+++ services/get_vendor.php
@@ -1,32 +1,32 @@
 <?php
+  // log errors if any
   ini_set('display_errors', 1);
   ini_set('log_errors', 1);
   ini_set('error_log', dirname(__FILE__) . '/error_log.txt');
+  // include database files
   include '../config/db.php';
-  //get vendor by their id
+
+  //function get vendor by their id
   function getVendor($id)
   {
-    global $con;
+    // global connection $con
+    global $con; 
     $query = 'select * from vendors where id = "$id" ';
-    $result = mysqli_query($con,$query);
-    
+    $result = mysqli_query($con, $query);
     if (!$result) 
       {
-	      $message = 'ERROR:' . mysql_error();
-	      return $message;
+        $message = 'ERROR:' . mysql_error();
+        return $message;
       }
-
     else
     {
-	    $rows = array();
-      while($row = mysqli_fetch_assoc($result)) 
+      $rows = array();
+      while ($row = mysqli_fetch_assoc($result)) 
       {
         $rows[] = $row;
       }
       print json_encode($rows);
     }
   }
-
   getVendor(1);
-    
-?>
+  ?>
